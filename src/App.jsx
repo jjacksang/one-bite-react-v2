add8ecb41304178a528290933bd3d8e7bfed8283
진상우 <tkddnrj321@naver.com>
2024-06-09 20:31:18 +0900

    setIsLoading 설정

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ function App() {
     useEffect(() => {
         const storedData = localStorage.getItem("diary");
         if (!storedData) {
+            setIsLoading(false);
             return;
         }
         const parsedData = JSON.parse(storedData);
